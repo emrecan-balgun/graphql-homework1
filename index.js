a99aaa02d430ed73b2d3ad3233a5fe50cc9fff67
Emrecan Balgün <deathlygrave@gmail.com>
2022-03-17 13:01:35 +0300

    update data and refactor typedefs and resolvers

--- index.js
+++ index.js
@@ -39,15 +39,19 @@ const typeDefs = gql`
   type Query {
     # Event
     events: [Event!]!
+    event(id: ID!): Event!
 
     # Location
     locations: [Location!]!
+    location(id: ID!): Location!
 
     # User
     users: [User!]!
+    user(id: ID!): User!
 
     # Participant
     participants: [Participant!]!
+    participant(id: ID!): Participant!
   }
 `;
 
@@ -55,15 +59,19 @@ const resolvers = {
     Query: {
       // Event
       events: () => events,
+      event: (parent, args) => events.find((event) => event.id === args.id),
 
       // Location
       locations: () => locations,
+      location: (parent, args) => locations.find((location) => location.id === args.id),
 
       // User
       users: () => users,
+      user: (parent, args) => users.find((user) => user.id === args.id),
 
       // Participant
       participants: () => participants,
+      participant: (parent, args) => participants.find((participant) => participant.id === args.id),
     },
 };
 
