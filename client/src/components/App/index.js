ed7e8c4a9622c131763ead2f044b890f0f8ec046
Emrecan Balgün <deathlygrave@gmail.com>
2022-04-06 17:09:38 +0300

    add list component from ant design

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,11 +1,42 @@
 import { Row, Col } from 'antd';
 import styles from './styles.module.css';
+
+import { List, Avatar } from 'antd';
+
+const data = [
+  {
+    title: 'Ant Design Title 1',
+  },
+  {
+    title: 'Ant Design Title 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+  },
+];
  
 function App() {
   return (
     <div className={styles.container}>
       <Row justify='center'>
-        <Col span={14} className={styles.content}>Col</Col>
+        <Col span={14} className={styles.content}>Col
+          <List
+            itemLayout="horizontal"
+            dataSource={data}
+            renderItem={item => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                  title={<a href="https://ant.design">{item.title}</a>}
+                  description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                />
+              </List.Item>
+              )}
+            />
+        </Col>
       </Row>
     </div>
   );
