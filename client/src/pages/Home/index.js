5a99857e22f8daad034f957ee3895bcc2217c906
Emrecan Balgün <deathlygrave@gmail.com>
2022-04-07 02:39:40 +0300

    add show events home page

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,34 +1,46 @@
-import React from 'react'
+import { List } from 'antd';
+import { useQuery } from '@apollo/client';
+import { GET_EVENTS } from './queries';
 
-import { List, Avatar } from 'antd';
+// const list = [
+//   {
+//     title: 'Ant Design Title 1',
+//   },
+//   {
+//     title: 'Ant Design Title 2',
+//   },
+//   {
+//     title: 'Ant Design Title 3',
+//   },
+//   {
+//     title: 'Ant Design Title 4',
+//   },
+// ];
 
-const data = [
-  {
-    title: 'Ant Design Title 1',
-  },
-  {
-    title: 'Ant Design Title 2',
-  },
-  {
-    title: 'Ant Design Title 3',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-];
+
 
 function Home() {
+    const { loading, error, data } = useQuery(GET_EVENTS);
+
+    if (loading) 
+        return <div>Loading...</div>;
+
+    if (error)
+    return <div>Error: {error.message}</div>
+
+    console.log(data.events);
+
   return (
         <List
             itemLayout="horizontal"
-            dataSource={data}
+            dataSource={data.events}
             renderItem={item => (
                 <List.Item>
                 <List.Item.Meta
-                    avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-                    title={<a href="https://ant.design">{item.title}</a>}
-                    description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                    title={item.title}
+                    description={item.desc}
                 />
+                <span>{item.date}</span>
                 </List.Item>
                 )}
         />
